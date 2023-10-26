1979e32e44112e6654f93851ddd2ac5c78bc718c
Jack W <jackweber943@gmail.com>
2023-10-26 13:05:06 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,1 +1,7 @@
-print("placeholder")
+begin = input("This is a basic two-player hangman game. Would you like to begin? Please enter 'yes' or 'no' ")
+if begin == "no":
+    print("Then why did you come here? ")
+
+if begin == "yes":
+    p1 = input("Great! Who is player 1? ")
+    p2 = input("And who is player 2? ")
