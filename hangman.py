b9fca727715e11964544432740fa21930526c7b3
Jack W <jackweber943@gmail.com>
2023-11-01 11:38:33 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,3 +1,4 @@
+import random
 begin = input("This is a basic two-player hangman game. Would you like to begin? Please enter 'yes' or 'no' ")
 if begin == "no":
     print("Then why did you come here? ")
@@ -11,6 +12,7 @@ if begin == "yes":
 word = input("Okay, " + p1 + " and " + p2 + ", let's begin. " + p1 + ", please enter a word. ")
 listword = list(word)
 underword = list(word)
+lenword = len(word)
 
 z = len(word)
 
@@ -19,8 +21,16 @@ while i < z:
     underword[i] = "_"
     i = i + 1
     
-g1 = input("Okay, " + p2 + ", please guess a letter. ")
-if g1 in listword:
-    print("Correct! " + g1 + " is in the word.")
+faillist = ["Nope, try again", "Sorry, try again", "FAIL, give it another shot", "Imagine guessing the wrong answer. Give it another shot"]
+lecontrol = 0
+
+while lecontrol <= lenword:
+    g1 = input("Okay, " + p2 + ", please guess a letter. ")
+    if g1 in listword:
+        print("Correct! " + g1 + " is in the word.")                  #This doesn't work, need to fix double letter words
+        lecontrol = lecontrol + 1
+    else:
+        randomfail = random.choice(faillist)
+        print(randomfail)
 
 #CREATE A LIST FOR BLANK SPACES AND USE A FUCNTUIB TI REMOVE THINGS FROM THE MAIN LIST
